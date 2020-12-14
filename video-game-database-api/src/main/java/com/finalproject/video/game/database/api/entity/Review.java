7b582eb059daba3278e33e06dea4bd338f8bb7a1
lcuevas6 <69730667+lcuevas6@users.noreply.github.com>
2020-12-13 20:09:20 -0700

    Added review entity

--- video-game-database-api/src/main/java/com/finalproject/video/game/database/api/entity/Review.java
+++ video-game-database-api/src/main/java/com/finalproject/video/game/database/api/entity/Review.java
@@ -1,5 +1,74 @@
 package com.finalproject.video.game.database.api.entity;
 
-public class Review {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Review {
+	
+	private Long id;
+	private String content;
+	private Date date;
+	private User user;
+	
+	@JsonIgnore
+	private VideoGameName game;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name = "gameId")
+	public VideoGameName getVideoGameName() {
+		return game;
+	}
+	
+	public void setGame(VideoGameName game) {
+		this.game = game;
+	}
+	
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	public User getUser() {
+		return user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 }
+
+
+
